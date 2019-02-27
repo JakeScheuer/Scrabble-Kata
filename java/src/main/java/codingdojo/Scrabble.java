5bbd0a0987b55d915400848ac780e6454415737f
JakeScheuer <scheuerjacob@gmail.com>
2019-02-27 11:13:58 -0500

    first test passed

--- java/src/main/java/codingdojo/Scrabble.java
+++ java/src/main/java/codingdojo/Scrabble.java
@@ -1,4 +1,12 @@
 package codingdojo;
 
 public class Scrabble {
+
+    public int score = 0;
+
+    public Character calculate(String word) {
+        Character firstLetter = word.toUpperCase().charAt(0);
+        return firstLetter;
+
+    }
 }
--- java/src/test/java/codingdojo/ScrabbleTest.java
+++ java/src/test/java/codingdojo/ScrabbleTest.java
@@ -1,12 +1,17 @@
 package codingdojo;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 public class ScrabbleTest
 {
     @Test
-    public void Foo()
-    {
-        // TODO: write this test!
+    public void whenCalculateIsPassedAStringReturnsFirstLetter() {
+
+        Scrabble scrabble = new Scrabble();
+        Character firstLetter = scrabble.calculate("foo");
+        assertEquals((Character)'F', firstLetter);
+
     }
 }
